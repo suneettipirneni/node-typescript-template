182c3018fc2e16dcef5dd0a217c566529fd2da24
suneettipirneni <suneettipirneni@icloud.com>
2021-05-22 01:26:10 -0400

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export function sayHello(): string {
+  return 'Hello World';
+}
+
+console.log(sayHello());
---
+++ src/test/test.ts
@@ -0,0 +1,5 @@
+import { sayHello } from '..';
+
+test('A sample test', () => {
+  expect(sayHello()).toBe('Hello World');
+});
